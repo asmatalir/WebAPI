8b499c2e82092e6160f383636b2bfd9e7cc881e0
Asmatali Rauthar <asmatali.rauthar@rheal.com>
2026-09-24 21:24:54 +0530

    Added GetAttendanceByID stored procedure

--- Database/Scripts/Attendance_sp.sql
+++ Database/Scripts/Attendance_sp.sql
@@ -6,3 +6,14 @@ BEGIN
     FROM Attendance;
 END
 GO
+
+CREATE OR ALTER PROCEDURE sp_GetAttendanceByID
+    @AttendanceID INT
+AS
+BEGIN
+    SELECT AttendanceID, StudentID, SubjectID, AttendanceDate, Status, IsActive,
+           CreatedBy, CreatedOn, ModifiedBy, ModifiedOn
+    FROM Attendance
+    WHERE AttendanceID = @AttendanceID AND IsActive=1;
+END
+GO
